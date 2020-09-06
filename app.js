2dc5ad9aafc0d722fbaaea7476d7b6c350d95f74
Sheba <sheba.aphilip@gmail.com>
2020-09-06 21:22:06 +1200

    refresh and basic functions

--- app.js
+++ app.js
@@ -12,6 +12,46 @@ const updateLocalStorage = (data) => {
   const serializedData = JSON.stringify(data);
   localStorage.setItem("localData", serializedData);
 };
+
+const getCellData = () => {
+  Object.keys(localData).map(
+    (key) =>
+      (document.querySelector(`#${key}`).textContent = localData[key].formula
+        ? localData[key].formula
+        : localData[key].value)
+  );
+};
+
+//on click of Refresh
+document.querySelector("#btnRefresh").addEventListener("click", () => {
+  let table = document.querySelector("#table");
+  table.removeChild(table.firstChild);
+  localData = initLocalStorage();
+  drawTable();
+  onCellInput();
+  getCellData();
+});
+// convert the alphaNumeric to Numeric
+const getNumericValue = (formula, regAlpha) =>
+  formula.replace(regAlpha, (key) =>
+    "formula" in localData[key] ? sheba[key].localData : localData[key].value
+  );
+
+// Basic functions
+const addFormula = (key, value) => {
+  const regAlpha = /[A-Z]\w*[0-9]/g;
+
+  let result = "";
+  const formula = value.substr(1).toUpperCase();
+  const numericValue = getNumericValue(formula, regAlpha);
+  result = eval(numericValue);
+
+  if (result) {
+    localData[key]["formula"] = result;
+    return result;
+  }
+};
+
 // convert numbers to alphabets
 const convertNumToAlpha = (number) => {
   let baseChar = "A".charCodeAt(0);
@@ -60,6 +100,9 @@ onCellInput = () => {
       if (ele.textContent) {
         localData[ele.id] = {};
         localData[ele.id]["value"] = ele.textContent;
+        if (ele.textContent.startsWith("=")) {
+          ele.textContent = addFormula(ele.id, ele.textContent);
+        }
         updateLocalStorage(localData);
       } else {
         updateLocalStorage(localData);
